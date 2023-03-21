9f787cd0c5c44dd671f7991188f2e8655c22c0a3
Gennadiy Popov <gennadiy.popov.87@yandex.ru>
2023-03-22 02:19:07 +0300

    feature: Add barely worked tests

---
+++ src/configWatcher.js
@@ -0,0 +1,194 @@
+const chokidar = require("chokidar");
+const path = require("node:path");
+const { glob } = require("glob");
+const throttle = require("lodash.throttle");
+const debounceCollect = require("debounce-collect");
+const { logger } = require("./logger");
+const fs = require("node:fs/promises");
+const dotenv = require("dotenv");
+const rimraf = require("rimraf");
+
+const configWatcher = (rootPath, resultPath, onChange) => {
+	const watcher = {
+		allFiles: {},
+		filesWatcher: undefined,
+		rootWatcher: undefined,
+	};
+
+	const filesChangesHandler = debounceCollect(async (events) => {
+		try {
+			logger.info(
+				"Files: " +
+					events
+						.map(([eventType, filePath]) => {
+							return `${eventType}: ${filePath};`;
+						})
+						.join("\n")
+			);
+
+			watcher.allFiles = events.reduce((result, [eventType, filePath]) => {
+				const [fileRoot, fileRelativePath] = filePath.split("current");
+				const fileBase = path.basename(fileRelativePath);
+
+				return {
+					...result,
+					[fileRoot]: {
+						...(result[fileRoot] || {}),
+						[fileBase]: [eventType, filePath],
+					},
+				};
+			}, watcher.allFiles);
+
+			const currentConfigs = Object.entries(watcher.allFiles).reduce(
+				(
+					result,
+					[root, { [".env"]: envFile, ["Caddyfile"]: caddyfile } = {}]
+				) => {
+					const status =
+						envFile[0] === "unlink" || caddyfile[0] === "unlink"
+							? "unlink"
+							: "exists";
+
+					return [...result, [root, status, envFile[1], caddyfile[1]]];
+				},
+				[]
+			);
+			let environment = {};
+
+			await Promise.all(
+				currentConfigs.map(
+					async ([root, status, envFilePath, caddyFilePath]) => {
+						const websiteBase = path.basename(root);
+						const configPath = path.resolve(resultPath, websiteBase);
+						const caddyFileOutPath = path.resolve(configPath, "Caddyfile");
+
+						if (status === "exists") {
+							await fs.mkdir(configPath, { recursive: true });
+							let caddyConfigContent = await fs.readFile(
+								caddyFilePath,
+								"utf-8"
+							);
+							const envFileContent = await fs.readFile(envFilePath, "utf-8");
+							const environmentPrefix = websiteBase
+								.toUpperCase()
+								.replace(/[\.-]/, "_");
+
+							const localEnvironment = Object.entries(
+								dotenv.parse(envFileContent)
+							).reduce(
+								(result, [key, value]) => {
+									const perfixedKey = `${environmentPrefix}_${key}`;
+									result.variables[perfixedKey] = value;
+									result.replaces[key] = perfixedKey;
+									return result;
+								},
+								{
+									variables: {},
+									replaces: {},
+								}
+							);
+
+							caddyConfigContent = Object.entries(
+								localEnvironment.replaces
+							).reduce(
+								(result, [key, replaceTo]) => result.replaceAll(key, replaceTo),
+								caddyConfigContent
+							);
+
+							environment = {
+								...environment,
+								[`---${environmentPrefix}---`]: [
+									environmentPrefix,
+									envFilePath,
+								],
+								...localEnvironment.variables,
+							};
+
+							await fs.writeFile(caddyFileOutPath, caddyConfigContent, "utf-8");
+						} else {
+							await rimraf(configPath);
+						}
+					}
+				)
+			);
+
+			const envFileContent = Object.entries(environment).reduce(
+				(result, [key, value]) => {
+					if (key.startsWith("---")) {
+						const [envPrefix, envPath] = value;
+						result = `${result}\n\n# ------------------------- ${envPrefix} -------------------------`;
+						result = `${result}\n# file: ${envPath}\n`;
+					} else {
+						result = `${result}\n${key}=${value}`;
+					}
+
+					return result;
+				},
+				""
+			);
+
+			await fs.writeFile(
+				path.resolve(resultPath, ".env"),
+				envFileContent,
+				"utf-8"
+			);
+
+			if (onChange) {
+				await onChange();
+			}
+		} catch (error) {
+			logger.error(error);
+		}
+	}, 100);
+
+	const rootWatcherFiles = [path.resolve(rootPath, "./**/current")];
+	watcher.rootWatcher = chokidar.watch(rootWatcherFiles, {
+		persistent: true,
+		ignoreInitial: false,
+		followSymlinks: true,
+		depth: 1,
+		ignore: "node_modules/**",
+	});
+
+	const startRootWatching = throttle(async () => {
+		const watchFiles = await glob([
+			path.resolve(rootPath, "./**/current"),
+			path.resolve(rootPath, "./**/current/docker/caddy/Caddyfile"),
+			path.resolve(rootPath, "./**/current/.env"),
+		]);
+
+		if (watcher.filesWatcher) {
+			watcher.filesWatcher.close();
+			watcher.filesWatcher = undefined;
+		}
+
+		watcher.filesWatcher = chokidar.watch(watchFiles, {
+			persistent: true,
+			depth: 2,
+			ignore: "node_modules/**",
+		});
+
+		watcher.filesWatcher.on("all", filesChangesHandler);
+	}, 300);
+
+	watcher.rootWatcher.on("all", startRootWatching);
+
+	watcher.close = function () {
+		startRootWatching.cancel();
+		if (this.rootWatcher) {
+			this.rootWatcher.close();
+			this.rootWatcher = undefined;
+		}
+
+		if (this.fileWatcher) {
+			this.filesWatcher.close();
+			this.filesWatcher = undefined;
+		}
+	}.bind(watcher);
+
+	return watcher;
+};
+
+module.exports = {
+	configWatcher,
+};
---
+++ tests/configWatcher.test.js
@@ -0,0 +1,325 @@
+const path = require("node:path");
+const dayjs = require("dayjs");
+const fs = require("node:fs/promises");
+const { default: rimraf } = require("rimraf");
+const { inspect } = require("node:util");
+const merge = require("lodash.merge");
+const { configWatcher } = require("../src/configWatcher");
+
+const ROOT_PATH = path.resolve(__dirname, "../test-data");
+const ROOT_CONFIG_OUT_PATH = path.resolve(ROOT_PATH, "out");
+const ROOT_LOCAL_CONFIGS_PATH = path.resolve(ROOT_PATH, "data");
+
+const DEFAULT_ENV_FILE_CONTENT_1 = `
+APP_HOST=acme.nope
+APP_TLS_EMAIL=support@acme.nope
+APP_BRANCH=production
+`;
+
+const DEFAULT_ENV_FILE_CONTENT_2 = `
+APP_HOST=acme-2.nope
+APP_TLS_EMAIL=support@acme-2.nope
+APP_BRANCH=development
+`;
+
+const DEFAULT_ENV_FILE_CONTENT_3 = `
+APP_HOST=acme-2.nope
+APP_TLS_EMAIL=support@acme-2.nope
+APP_BRANCH=development
+`;
+
+const DEFAULT_CADDY_CONFIG_CONTENT = `
+{$APP_HOST:acme.net}.localhost, {$APP_HOST:acme.net} {
+    root * /var/www/website-{$APP_BRANCH:production}/current/public
+    tls {$APP_TLS_EMAIL:internal}
+}
+`;
+
+const SETTINGS_STARTS_REGEXP = /^#/;
+
+const getEntryInfo = (pathInfo) => {
+	const result = Object.entries(pathInfo).reduce(
+		(result, [key, value]) => {
+			if (key.startsWith("#")) {
+				const settingsKey = key.replace(SETTINGS_STARTS_REGEXP, "");
+				result.settings[settingsKey] = value;
+			} else {
+				result.entries[key] = value;
+			}
+			return result;
+		},
+		{
+			settings: {},
+			entries: {},
+		}
+	);
+
+	result.settings.type = result.settings.type ?? "directory";
+
+	return result;
+};
+
+const treeToFlatFSStructureReducer = (
+	{ rootPath, flatFsStructure: prevFsStructure = [] },
+	[localPath, combinedContent]
+) => {
+	const { settings, entries } = getEntryInfo(combinedContent);
+
+	const fullPath = path.resolve(rootPath, localPath);
+
+	let nestedFlatFsStructure = {};
+	if (settings.type === "directory") {
+		nestedFlatFsStructure = Object.entries({
+			...entries,
+		}).reduce(treeToFlatFSStructureReducer, {
+			rootPath: fullPath,
+			flatFsStructure: {},
+		}).flatFsStructure;
+	}
+
+	const flatFsStructure = {
+		...prevFsStructure,
+		[fullPath]: { ...settings },
+		...nestedFlatFsStructure,
+	};
+
+	return {
+		rootPath,
+		flatFsStructure,
+	};
+};
+
+const applySettingsToPath = async (targetPath, settings = {}) => {
+	let pathInfo = null;
+	try {
+		pathInfo = await fs.lstat(targetPath);
+	} catch (error) {}
+
+	switch (settings.type) {
+		case "absent":
+			await rimraf(targetPath);
+			break;
+
+		case "directory":
+			if (settings.clear) {
+				await rimraf(targetPath);
+			}
+
+			await fs.mkdir(targetPath, { recursive: true });
+			break;
+
+		case "file":
+			if (pathInfo !== null) {
+				await fs.unlink(targetPath);
+			}
+			await fs.writeFile(targetPath, settings.content);
+			break;
+
+		case "link":
+			if (pathInfo !== null) {
+				await fs.unlink(targetPath);
+			}
+
+			await fs.symlink(settings.to, targetPath);
+			break;
+	}
+};
+
+const makeFsStructure = async (rootPath, fsStructure) => {
+	const { settings, entries } = getEntryInfo(fsStructure);
+
+	const pathMap = {
+		[rootPath]: { ...settings },
+		...(await Object.entries(entries).reduce(treeToFlatFSStructureReducer, {
+			rootPath,
+			flatFsStructure: {},
+		}).flatFsStructure),
+	};
+
+	const pathList = [];
+
+	for (const [localPath, config] of Object.entries(pathMap)) {
+		await applySettingsToPath(localPath, config);
+		pathList.push(localPath);
+	}
+
+	return pathList;
+};
+
+const makeReleaseInfoFiles = (releaseName, caddy, env) => {
+	return {
+		".env": {
+			"#type": "file",
+			"#content": env,
+		},
+		docker: {
+			caddy: {
+				Caddyfile: {
+					"#type": "file",
+					"#content": caddy,
+				},
+			},
+		},
+		release: {
+			"#type": "file",
+			"#content": releaseName,
+		},
+	};
+};
+
+const makeReleaseName = (releaseShift = 0) =>
+	dayjs(Date.now() + releaseShift).format("YYYYMMDDHHmmssSSS");
+
+const makeNewRelease = (siteName, releaseShift = 0, caddy, env) => {
+	const currentRelease = makeReleaseName(releaseShift);
+
+	return {
+		[siteName]: {
+			releases: {
+				[currentRelease]: makeReleaseInfoFiles(currentRelease, caddy, env),
+			},
+		},
+	};
+};
+
+const makeWebsiteDeploy = (websiteName, releases = [], clear = false) => {
+	const fullReleases = [];
+	for (const realseArgs of releases) {
+		fullReleases.push(makeNewRelease(websiteName, ...realseArgs));
+	}
+
+	const websiteFSStruture = {
+		[websiteName]: {
+			"#clear": clear,
+			current: {
+				"#type": "link",
+				"#to": `releases/${
+					Object.entries(fullReleases[0][websiteName].releases)[0][0]
+				}`,
+			},
+		},
+	};
+
+	return merge(websiteFSStruture, ...fullReleases);
+};
+
+async function initializeDefaultFileStructure() {
+	const websiteProdInitialDeploy = makeWebsiteDeploy(
+		"website-production",
+		[
+			[0, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_1],
+			[5436, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_1],
+			[12345, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_1],
+		],
+		true
+	);
+
+	await makeFsStructure(ROOT_CONFIG_OUT_PATH, {
+		"#clear": true,
+	});
+
+	return await makeFsStructure(
+		ROOT_LOCAL_CONFIGS_PATH,
+		websiteProdInitialDeploy
+	);
+}
+
+async function clearTestDataStructure() {
+	await makeFsStructure(ROOT_PATH, {
+		"#clear": true,
+	});
+}
+
+describe("Config watcher", () => {
+	let watcher = null;
+	beforeAll(async () => {
+		await initializeDefaultFileStructure();
+		watcher = configWatcher(ROOT_LOCAL_CONFIGS_PATH, ROOT_CONFIG_OUT_PATH);
+	});
+
+	it("should global settings settings by local at startup", async () => {
+		await new Promise((r) => setTimeout(r, 2000));
+	});
+
+	it("should regenerate configurations after added new website", async () => {
+		const websiteNewDeploy = makeWebsiteDeploy("website-stage", [
+			[0, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_1],
+			[5436, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_1],
+			[12345, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_1],
+		]);
+
+		await makeFsStructure(ROOT_LOCAL_CONFIGS_PATH, websiteNewDeploy);
+
+		await new Promise((r) => setTimeout(r, 2000));
+	});
+
+	it("should regenerate configurations after website removed", async () => {
+		await makeFsStructure(ROOT_LOCAL_CONFIGS_PATH, {
+			"website-production": {
+				"#type": "absent",
+			},
+		});
+
+		await new Promise((r) => setTimeout(r, 1000));
+	});
+
+	it("should regenerate configurations after website removed", async () => {
+		await makeFsStructure(ROOT_LOCAL_CONFIGS_PATH, {
+			"website-stage": {
+				current: {
+					"#type": "absent",
+				},
+			},
+		});
+
+		await new Promise((r) => setTimeout(r, 1000));
+	});
+
+	it("should regenerate configurations after local website settings changed", () => {});
+
+	it("should regenerate configurations after link to release changed", async () => {
+		const websiteName = "website-test";
+
+		const websiteProdInitialDeploy = makeWebsiteDeploy(websiteName, [
+			[0, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_1],
+			[5436, DEFAULT_CADDY_CONFIG_CONTENT, DEFAULT_ENV_FILE_CONTENT_2],
+		]);
+
+		await makeFsStructure(ROOT_LOCAL_CONFIGS_PATH, websiteProdInitialDeploy);
+		await new Promise((r) => setTimeout(r, 1000));
+
+		const newRelease = makeNewRelease(
+			websiteName,
+			0,
+			DEFAULT_CADDY_CONFIG_CONTENT,
+			DEFAULT_ENV_FILE_CONTENT_3
+		);
+
+		await makeFsStructure(ROOT_LOCAL_CONFIGS_PATH, newRelease);
+
+		await new Promise((r) => setTimeout(r, 1000));
+		console.log(
+			"---------------------------------------------------------------------"
+		);
+
+		await makeFsStructure(ROOT_LOCAL_CONFIGS_PATH, {
+			[websiteName]: {
+				current: {
+					"#type": "link",
+					"#to": `releases/${Object.entries(newRelease[websiteName].releases)
+						.at(0)
+						.at(0)}`,
+				},
+			},
+		});
+
+		await new Promise((r) => setTimeout(r, 1000));
+	});
+
+	afterAll(async () => {
+		if (watcher) {
+			watcher.close();
+		}
+		// await clearTestDataStructure();
+	});
+});
